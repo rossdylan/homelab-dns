e58e25e76fe854542f59e43a9843b8ca17978cc1
Ross Delinger <rossdylan@fastmail.com>
2022-03-20 17:06:49 -0700

    [controller] Reimplement main loop as a kube-rs controller
    Rather than have a big monolithic main function I've split things out
    and started using the controller pattern. The DNS server is spawned, and
    then we tell kube-rs to start a controller loop for Service objects.
    
    Our reconciliation loop is a bit complicated since we aren't technically
    the owner of Service objects. We still use finalizers but instead of 2
    cases (Apply, Cleanup) we have 3 cases.
    1. A service is added/updated with our attribute. In which case the
       logic is basically the same as before.
    2. A service with our attribute is deleted. Here we use kube-rs's
       finalizer support to automatically handle the cleanup.
    3. A service with our attribute is updated to remove the attribute.
       This case is tricky, but we handle it mostly the same as case 2.
       Instead of automatically removing the finalizer via kube-rs we
       do that part ourselves after running the normal cleanup routine
    
    The code is still kinda messy but generally speaking the architecture if
    a bit better then before. The next step is to do some cleanups and write
    the deployment manifests.

--- src/main.rs
+++ src/main.rs
@@ -14,23 +14,27 @@
     unreachable_pub
 )]
 
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
 use std::net::{IpAddr, SocketAddr};
 use std::str::FromStr;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
 use anyhow::anyhow;
 use clap::Parser;
-use futures::{pin_mut, TryStreamExt};
+use futures::StreamExt;
+use humantime::Duration as HumanDuration;
+use json_patch::{PatchOperation, RemoveOperation, TestOperation};
 use k8s_openapi::api::core::v1::Service;
+use kube::api::{Patch, PatchParams};
+use kube::runtime::finalizer;
 use kube::{
-    api::{Api, ListParams},
-    runtime::{reflector, utils::try_flatten_applied, watcher},
-    Client,
+    runtime::controller::{Action, Context, Controller},
+    runtime::finalizer::{Error as FinalizerError, Event},
+    Api, Client, Resource, ResourceExt,
 };
 use tokio::net::{TcpListener, UdpSocket};
-use tracing::{error, info};
+use tracing::{debug, error, info};
 use trust_dns_client::rr::{LowerName, RrKey};
 use trust_dns_proto::rr::rdata::soa::SOA;
 use trust_dns_proto::rr::{Name, RData, Record, RecordSet, RecordType};
@@ -42,8 +46,66 @@ use trust_dns_server::ServerFuture;
 /// should be associated with
 const HOMELAB_DNS_ANNOTATION: &str = "k8s.r0ssd.co/dns-entry";
 
+/// The label we use configure services with an additional finalizer so we can remove DNS records
+const HOMELAB_DNS_FINALIZER: &str = "homelab-dns.k8s.r0ssd.co/cleanup";
+
+#[derive(thiserror::Error, Debug)]
+enum HLDError {
+    #[error("dns name conversion failure")]
+    ProtoError(#[from] trust_dns_proto::error::ProtoError),
+
+    #[error("no namespace found in object")]
+    NoNamespace,
+
+    #[error("kube-rs failure")]
+    KubeError(#[from] kube::Error),
+
+    #[error("finalizer failed to apply")]
+    FinalizerApplyFailed {
+        #[source]
+        source: Box<HLDError>,
+    },
+    #[error("finalizer cleanup failed")]
+    FinalizerCleanupFailed {
+        #[source]
+        source: Box<HLDError>,
+    },
+    #[error("failed to add finalizer")]
+    FinalizerAddFailed {
+        #[source]
+        source: kube::Error,
+    },
+    #[error("failed to add finalizer")]
+    FinalizerRemoveFailed {
+        #[source]
+        source: kube::Error,
+    },
+    #[error("unnamed object")]
+    FinalizerUnnamedObject,
+}
+
+impl From<FinalizerError<Self>> for HLDError {
+    fn from(error: FinalizerError<Self>) -> Self {
+        match error {
+            FinalizerError::ApplyFailed(inner) => HLDError::FinalizerApplyFailed {
+                source: Box::new(inner),
+            },
+            FinalizerError::CleanupFailed(inner) => HLDError::FinalizerCleanupFailed {
+                source: Box::new(inner),
+            },
+            FinalizerError::AddFinalizer(inner) => HLDError::FinalizerAddFailed { source: inner },
+            FinalizerError::RemoveFinalizer(inner) => {
+                HLDError::FinalizerRemoveFailed { source: inner }
+            }
+            FinalizerError::UnnamedObject => HLDError::FinalizerUnnamedObject,
+        }
+    }
+}
+
+type HLDResult<T, E = HLDError> = std::result::Result<T, E>;
+
 /// External-ish DNS for homelab kubernetes clusters
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Clone)]
 #[clap(author = "Ross Delinger <rossdylan@fastmail.com>", version = "0.1", about, long_about = None)]
 struct Args {
     /// The root domain we are serving DNS for.
@@ -54,10 +116,11 @@ struct Args {
     #[clap(short, long, default_value = "127.0.0.1:53")]
     bind: SocketAddr,
 
-    // Time to set on TCP dns lookups in seconds
-    #[clap(long, default_value_t = 1)]
-    tcp_timeout_secs: u64,
+    /// Timeout for DNS over TCP connections.
+    #[clap(long, default_value = "1s")]
+    tcp_timeout: HumanDuration,
 
+    /// Set the TTL of the LoadBalancer DNS records
     #[clap(short, long, default_value_t = 300)]
     ttl: u32,
 }
@@ -110,13 +173,8 @@ fn generate_initial_records(origin: &Name) -> BTreeMap<RrKey, RecordSet> {
     map
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
-
-    tracing_subscriber::fmt::init();
-
-    // This part is all trust-dns-server configuration
+/// Create our DNS server and return the underlying zone we use to update entries with
+async fn spawn_server(args: &Args) -> anyhow::Result<Arc<InMemoryAuthority>> {
     let recs = generate_initial_records(&args.origin);
     let authority = Arc::new(
         InMemoryAuthority::new(args.origin.clone(), recs, ZoneType::Primary, false)
@@ -128,80 +186,254 @@ async fn main() -> anyhow::Result<()> {
 
     let mut server = ServerFuture::new(catalog);
     let udp_sock = UdpSocket::bind(args.bind).await?;
+    tracing::info!("dns udp bound to {}", udp_sock.local_addr().unwrap());
     let tcp_listener = TcpListener::bind(args.bind).await?;
+    tracing::info!("dns tcp bound to {}", tcp_listener.local_addr().unwrap());
 
     server.register_socket(udp_sock);
-    server.register_listener(tcp_listener, Duration::from_secs(args.tcp_timeout_secs));
+    server.register_listener(tcp_listener, *args.tcp_timeout);
     tokio::task::spawn(server.block_until_done());
+    Ok(authority)
+}
 
+/// State passed to our reconciler function via the kube-rs Controller. It contains a reference to our
+/// DNS authority and general application configuration used to create new entries.
+struct ReconcilerState {
+    authority: Arc<InMemoryAuthority>,
+    service_to_domain: Arc<Mutex<HashMap<String, String>>>,
+    kube: Client,
+    args: Args,
+}
+
+impl ReconcilerState {
+    fn new(authority: Arc<InMemoryAuthority>, args: Args, kube: Client) -> Self {
+        ReconcilerState {
+            authority,
+            service_to_domain: Arc::new(Mutex::new(HashMap::new())),
+            kube,
+            args,
+        }
+    }
+
+    fn domain_for_service(&self, service: &str) -> Option<String> {
+        self.service_to_domain
+            .lock()
+            .unwrap()
+            .get(service)
+            .map(Clone::clone)
+    }
+
+    fn remove_domain_for_service(&self, service: &str) {
+        self.service_to_domain.lock().unwrap().remove(service);
+    }
+
+    fn set_domain_for_service(&self, service: &str, domain: &str) {
+        self.service_to_domain
+            .lock()
+            .unwrap()
+            .insert(service.into(), domain.into());
+    }
+}
+
+/// Run a single instance of our reconciliation loop. We check to see if the changed service is one we
+/// should care about, then use the kube-rs finalizer support to either apply new updates to our
+/// authoritative zone, or remove the relevant records.
+async fn reconcile(service: Arc<Service>, ctx: Context<ReconcilerState>) -> HLDResult<Action> {
+    // The first thing we do is check to see if we care about this Service.
+    // This way we don't attach finalizers to all services
+    let is_lb = if let Some(spec) = &service.spec {
+        matches!(spec.type_.as_deref(), Some("LoadBalancer"))
+    } else {
+        false
+    };
+    let lb_name = service.metadata.name.as_deref().unwrap_or("???");
+    if !is_lb {
+        debug!(
+            "ignoring service {} because it is not a LoadBlanacer",
+            lb_name
+        );
+        return Ok(Action::requeue(Duration::from_secs(3600)));
+    }
+    let state = ctx.get_ref();
+    let anno = get_hld_annotation(&service);
+    let previous_anno = state.domain_for_service(lb_name);
+    let ns = service
+        .meta()
+        .namespace
+        .as_deref()
+        .ok_or(HLDError::NoNamespace)?;
+    let sapi: Api<Service> = Api::namespaced(ctx.get_ref().kube.clone(), ns);
+
+    if anno.is_none() && previous_anno.is_none() {
+        debug!(
+            "ignoring service {} because it is not marked with '{}'",
+            lb_name, HOMELAB_DNS_ANNOTATION,
+        );
+        return Ok(Action::requeue(Duration::from_secs(3600)));
+    }
+    // Handle our early termination case where something has removed the annotation from a service but not deleted it completely
+    if anno.is_none() && previous_anno.is_some() {
+        return reconcile_early_cleanup(sapi, service, ctx).await;
+    }
+
+    let action = finalizer(&sapi, HOMELAB_DNS_FINALIZER, service, |event| async {
+        match event {
+            Event::Apply(svc) => reconcile_apply(svc, ctx).await,
+            Event::Cleanup(svc) => reconcile_cleanup(svc, ctx).await,
+        }
+    })
+    .await?;
+    Ok(action)
+}
+
+/// This cleanup helper is used to remove DNS entries when something has removed the homelab-dns annotation from a service
+async fn reconcile_early_cleanup(
+    sapi: Api<Service>,
+    service: Arc<Service>,
+    ctx: Context<ReconcilerState>,
+) -> HLDResult<Action> {
+    reconcile_cleanup(service.clone(), ctx).await?;
+    let finalizer_index = &service
+        .finalizers()
+        .iter()
+        .enumerate()
+        .find(|(_, fin)| *fin == HOMELAB_DNS_FINALIZER)
+        .map(|(i, _)| i);
+    if let Some(fi) = finalizer_index {
+        let name = service
+            .meta()
+            .name
+            .clone()
+            .ok_or(HLDError::FinalizerUnnamedObject)?;
+
+        let finalizer_path = format!("/metadata/finalizers/{}", fi);
+        sapi.patch::<Service>(
+            &name,
+            &PatchParams::default(),
+            &Patch::Json(json_patch::Patch(vec![
+                // All finalizers run concurrently and we use an integer index
+                // `Test` ensures that we fail instead of deleting someone else's finalizer
+                // (in which case a new `Cleanup` event will be sent)
+                PatchOperation::Test(TestOperation {
+                    path: finalizer_path.clone(),
+                    value: HOMELAB_DNS_FINALIZER.into(),
+                }),
+                PatchOperation::Remove(RemoveOperation {
+                    path: finalizer_path,
+                }),
+            ])),
+        )
+        .await
+        .map_err(|e| HLDError::FinalizerRemoveFailed { source: e })?;
+    }
+    Ok(Action::await_change())
+}
+
+async fn reconcile_cleanup(
+    service: Arc<Service>,
+    ctx: Context<ReconcilerState>,
+) -> HLDResult<Action> {
+    let state = ctx.get_ref();
+    let mut records = state.authority.records_mut().await;
+
+    let lb_name = service.metadata.name.as_deref().unwrap_or("???");
+    // Grab the domain name from the current annotation, or the last value we observed
+    let anno = get_hld_annotation(&service).or_else(|| state.domain_for_service(lb_name));
+    if anno.is_none() {
+        debug!(
+            "ignoring service {} because it is not marked with '{}'",
+            lb_name, HOMELAB_DNS_ANNOTATION,
+        );
+        return Ok(Action::await_change());
+    }
+
+    let name = Name::from_str(&anno.unwrap())?;
+
+    let a_key = RrKey::new(LowerName::new(&name), RecordType::A);
+    let aaaa_key = RrKey::new(LowerName::new(&name), RecordType::AAAA);
+    if let Some(a_records) = records.remove(&a_key) {
+        info!(
+            "removing A records {:?}",
+            a_records.records_without_rrsigs()
+        );
+    }
+    if let Some(aaaa_records) = records.remove(&aaaa_key) {
+        info!(
+            "removing AAAA records {:?}",
+            aaaa_records.records_without_rrsigs()
+        );
+    }
+    state.remove_domain_for_service(lb_name);
+    Ok(Action::await_change())
+}
+
+async fn reconcile_apply(
+    service: Arc<Service>,
+    ctx: Context<ReconcilerState>,
+) -> HLDResult<Action> {
+    let state = ctx.get_ref();
+    let anno = get_hld_annotation(&service).unwrap();
+    let lb_name = service.metadata.name.as_deref().unwrap_or("???");
+    let name = Name::from_str(&anno)?;
+
+    if let Some(addrs) = get_lb_ips(&service) {
+        // TODO(rossdylan): We don't handle the case where the assigned addresses have been changed.
+        // This will require some deeper twiddling of the authority database to remove old records
+        for addr in addrs {
+            let parsed_addr = match addr.parse::<IpAddr>() {
+                Err(e) => {
+                    error!("failed to parse IP Address for service {}: {}", lb_name, e);
+                    continue;
+                }
+                Ok(pa) => pa,
+            };
+            let rdata = match parsed_addr {
+                IpAddr::V4(v4) => RData::A(v4),
+                IpAddr::V6(v6) => RData::AAAA(v6),
+            };
+            let success = state
+                .authority
+                .upsert(Record::from_rdata(name.clone(), state.args.ttl, rdata), 0)
+                .await;
+            if success {
+                info!(
+                    "added record for service {}: {} -> {}",
+                    lb_name, name, parsed_addr
+                );
+            } else {
+                info!(
+                    "failed to add record for service {}: {} -> {}",
+                    lb_name, name, parsed_addr
+                );
+            }
+        }
+    }
+    state.set_domain_for_service(lb_name, &anno);
+    Ok(Action::requeue(Duration::from_secs(3600)))
+}
+
+fn error_policy(error: &HLDError, _ctx: Context<ReconcilerState>) -> Action {
+    error!("reconciliation failed: {}", error);
+    Action::requeue(Duration::from_secs(5))
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    tracing_subscriber::fmt::init();
+
+    let authority = spawn_server(&args).await?;
+
+    // This part is all trust-dns-server configuration
     // All the kube reflector shit is below this point
     let client = Client::try_default().await?;
-    let services: Api<Service> = Api::all(client);
-    let lp = ListParams::default();
-    let store = reflector::store::Writer::<Service>::default();
-    // TODO(rossdylan): Spin off a background task to read through our state every 15-30s and handle deletions
-    let reader = store.as_reader();
+    let services: Api<Service> = Api::all(client.clone());
 
-    let rf = reflector(store, watcher(services, lp));
-    let current = reader.state();
-    for service in current {
-        info!("initial read of service: {:?}", *service)
-    }
-    let sw = try_flatten_applied(rf);
-    pin_mut!(sw);
-    while let Some(service) = sw.try_next().await? {
-        let is_lb = if let Some(spec) = &service.spec {
-            matches!(spec.type_.as_deref(), Some("LoadBalancer"))
-        } else {
-            false
-        };
-        let lb_name = service.metadata.name.as_deref().unwrap_or("???");
-
-        if !is_lb {
-            continue;
-        }
-
-        let anno = get_hld_annotation(&service);
-        if anno.is_none() {
-            continue;
-        }
-        let name = match Name::from_str(&anno.unwrap()) {
-            Err(e) => {
-                error!("annotated domain has invalid format: {}", e);
-                continue;
-            }
-            Ok(n) => n,
-        };
-
-        if let Some(addrs) = get_lb_ips(&service) {
-            for addr in addrs {
-                let parsed_addr = match addr.parse::<IpAddr>() {
-                    Err(e) => {
-                        error!("failed to parse IP Address for service {}: {}", lb_name, e);
-                        continue;
-                    }
-                    Ok(pa) => pa,
-                };
-                let rdata = match parsed_addr {
-                    IpAddr::V4(v4) => RData::A(v4),
-                    IpAddr::V6(v6) => RData::AAAA(v6),
-                };
-                let success = authority
-                    .upsert(Record::from_rdata(name.clone(), args.ttl, rdata), 0)
-                    .await;
-                if success {
-                    info!(
-                        "added record for service {}: {} -> {}",
-                        lb_name, name, parsed_addr
-                    );
-                } else {
-                    info!(
-                        "failed to add record for service {}: {} -> {}",
-                        lb_name, name, parsed_addr
-                    );
-                }
-            }
-        }
-    }
+    let state = ReconcilerState::new(authority, args, client);
+    Controller::new(services, Default::default())
+        .run(reconcile, error_policy, Context::new(state))
+        .for_each(|_| futures::future::ready(()))
+        .await;
     Ok(())
 }
